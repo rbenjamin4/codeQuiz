62b5c15cd889d971f5e5ad9c943c172ffe42b678
Reed Benjamin <reed@Reeds-MacBook-Pro.local>
2023-12-13 00:15:36 -0500

    Updated HTML layout, added some basic spacing and styling to css, created quiz questions and function to pull from question objects

--- app.js
+++ app.js
@@ -1,13 +1,87 @@
 // click button to start game
 
+// const button = document.querySelector('#start')
+// let quizContainer = document.querySelector('#questionsContainer');
+// let answerOptions = document.querySelector('#answerOptions')
+// let resultsContainer = document.querySelector('#results');
+// let timer = document.querySelector('#timer')
+
+let submitButton = document.querySelector('#submit');
+
+const questionEl = document.querySelector('#question')
+
+const a_text = document.querySelector('#a_text')
+const b_text = document.querySelector('#b_text')
+const c_text = document.querySelector('#c_text')
+
+let currentQuiz = 0
+
+// questions and answer options
+
+const quizData = [
+    {
+        question: "What does HTML stand for?",
+        a: 'Hyper Tension Maker Limited',
+        b: 'HyperText Markup Language',
+        c: 'How The Markup Loads',
+        correct: 'b'
+    },
+    {
+        question: "What does CSS stand for?",
+        a: 'Centralized Style System',
+        b: 'Class System Sheet',
+        c: 'Cascading Style Sheet',
+        correct: 'c'
+    }
+]
+
+let score = 0
 
 
 
-// create event listener and function to start timer and present question
+// function to start timer and present question
+const startQuiz = () => {
+
+const currentQuizData = quizData[currentQuiz]    
+questionEl.textContent = currentQuizData.question
+a_text.textContent = currentQuizData.a
+b_text.textContent = currentQuizData.b
+c_text.textContent = currentQuizData.c
+
+currentQuiz++
+
+// timer
+    // let duration = 10
+    // const countdown = () => {
+    //     duration --
+    //     if (duration === 0){
+    //         alert('You\'re out of time!')
+    //     }
+    // }
+    // let timerInterval = window.setInterval(countdown, 1000)
+
+// display questions
+    // let quizPageEl = document.querySelector('#quizContainer') 
+    // quizPageEl.setAttribute("class", "hide")
+    // questionsEl.removeAttribute("class") 
+    // getQuestion()
 
 
 
-//present answer options
+
+}
+
+
+// create event listener to start quiz and load next question
+submitButton.addEventListener('click', () => {
+
+currentQuiz++
+
+startQuiz()
+
+
+})
+
 
 
 
