a618bd1477b45813dee62586fb2bd303a6b97582
Reed Benjamin <reed@Reeds-MacBook-Pro.local>
2023-12-14 14:47:08 -0500

    Added timer with display and countdown deduction for incorrect answers

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ const questionEl = document.querySelector('#question')
 submitButton = document.querySelector('#submit')
 const answerEls = document.querySelectorAll('.answer')
 const quizContainer = document.querySelector('#quizContainer')
+const timerDisplay = document.querySelector('#timer')
+const startButton = document.querySelector('#start')
+
 
 const a_text = document.querySelector('#a_text')
 const b_text = document.querySelector('#b_text')
@@ -28,9 +31,8 @@ const c_text = document.querySelector('#c_text')
 
 let currentQuiz = 0
 let score = 0
+let duration = 30
 
-
-loadQuiz()
 
 // start quiz and load question data
 function loadQuiz() {
@@ -42,6 +44,17 @@ function loadQuiz() {
     a_text.textContent = currentQuestionData.a
     b_text.textContent = currentQuestionData.b
     c_text.textContent = currentQuestionData.c
+
+    const countdown = () => {
+        duration--
+        timerDisplay.textContent = 'time: ' + duration
+        if (duration === 0){
+            alert('You\'re out of time!')
+            clearInterval(timer)
+        }
+    }
+
+    let timer = window.setInterval(countdown, 1000)
 
 }
 
@@ -69,6 +82,9 @@ submitButton.addEventListener('click', () => {
     if(answer) {
         if(answer === questionData[currentQuiz].correct){
             score++
+        }else {
+            duration -= 5
+            timerDisplay.textContent = 'time: ' + duration
         }
         
         currentQuiz++
@@ -82,3 +98,5 @@ submitButton.addEventListener('click', () => {
     }  
 
 })
+
+startButton.addEventListener('click', loadQuiz)
